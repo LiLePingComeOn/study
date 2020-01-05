690ab8757641f2049abb41ab04c002802a246d8e
LiLePingComeOn <2056244574@qq.com>
2020-01-05 16:30:44 +0800

    --解决冲突

--- src/study/Test.java
+++ src/study/Test.java
@@ -2,6 +2,8 @@ package study;
 
 public class Test {
      public static void test(){
+       System.out.println("GitHub");
        System.out.println("HaHa");
+       System.out.println("localhost");
      }
 }
